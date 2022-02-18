fb8c2e0b2906c8d125bbebb78ffec67431eb100b
Oussama-Lamhamdi-Alaoui <olamhamdialaoui@gmail.com>
2022-02-18 21:06:09 +0100

    changed index.js to use the env variables wrapper and fixed db connection bug

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import app from './app.js'
 import connectToMongo from './mongo.js'
+import * as config from './config.js'
 
-const PORT = process.env.PORT || 4000
+const { server: serverConfig } = config
 
 async function startServer() {
-  await connectToMongo
-  app.listen(PORT, () => {
-    console.log(`Server running http://localhost:${PORT} 🚀`)
+  await connectToMongo()
+  app.listen(serverConfig.port, () => {
+    console.log(`Server running http://localhost:${serverConfig.port} 🚀`)
   })
 }
 
