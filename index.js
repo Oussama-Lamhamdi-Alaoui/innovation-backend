81ac53b7bee33ea8a70329cc150049ab602a5f34
Oussama-Lamhamdi-Alaoui <olamhamdialaoui@gmail.com>
2022-02-19 13:54:22 +0100

    fixed http server using wrong port

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ import {
 } from 'apollo-server-core'
 import http from 'http'
 
+import * as config from './config.js'
 import app from './app.js'
 import connectToMongo from './mongo.js'
 import typeDefs from './schema.js'
 import resolvers from './resolvers.js'
+
+const { server: serverConfig } = config
 
 async function startServer() {
   await connectToMongo()
@@ -26,8 +29,14 @@ async function startServer() {
 
   await server.start()
   server.applyMiddleware({ app })
-  await new Promise((resolve) => httpServer.listen({ port: 8080 }, resolve))
-  console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`)
+
+  await new Promise((resolve) =>
+    httpServer.listen({ port: serverConfig.port }, resolve)
+  )
+
+  console.log(
+    `🚀 Server ready at http://localhost:${serverConfig.port}${server.graphqlPath}`
+  )
 }
 
 startServer()
