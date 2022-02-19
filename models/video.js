7fdd46f4e399b5affa69b57b8710b7cfb65d0b4c
Ayoublaachir <laachir1998@hotmail.com>
2022-02-19 06:22:44 -0800

    added video model

---
+++ models/video.js
@@ -0,0 +1,45 @@
+import pkg from 'mongoose'
+const { Schema, model } = pkg
+
+const VideoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+    length: {
+      type: Number,
+    },
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { collection: 'video', timestamps: true }
+)
+
+export const Video = model('Video', VideoSchema)
+
+export const getVideoById = async (id) => Video.findOne({ _id: id }).lean()
+
+export const getVideosByOwnerId = async (id) => Video.find({ owner: id }).lean()
+
+export const createVideo = async ({
+  title,
+  description,
+  owner,
+  thumbnail,
+  length,
+}) => {
+  const video = await Video.create({
+    title,
+    description,
+    owner: owner._id,
+    thumbnail,
+    length,
+  })
+  return video
+}
